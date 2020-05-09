d4d3745b093c5a2b4d597ffde09f398b330fb43e
admin <wxyhhy911@gmail.com>
2020-05-10 07:15:11 +0800

    play

--- w/js/common.js
+++ w/js/common.js
@@ -1,1 +1,13 @@
-function n(){}
+
+var docHeight=document.documentElement.clientHeight || document.body.clientHeight;
+var height=docHeight-200;
+var width=750;
+function popup(url,width,height){
+    var top = (window.screen.height-30-height)/2;       //获得窗口的垂直位置;
+    var left = (window.screen.width-10-width)/2;        //获得窗口的水平位置;
+    window.open(url,'video','height='+height+',,innerHeight='+height+',width='+width+',innerWidth='+width+',top='+top+',left='+left+',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+}
+function popPlay(id){
+    popup('/api/play?id='+id,videoWidth,videoHeight);
+    return false;
+}
--- w/js/video_play.js
+++ w/js/video_play.js
@@ -1,6 +1,4 @@
-var docHeight=document.documentElement.clientHeight || document.body.clientHeight;
-var height=docHeight-200;
-var width=750;
+
 
 $('.open-video a').click(function(){
     return false
@@ -68,12 +66,3 @@ $('#video-modal').on('hidden.bs.modal', function (e) {
         player.pause();
     });
 })
-function popup(url,width,height){
-    var top = (window.screen.height-30-height)/2;       //获得窗口的垂直位置;
-    var left = (window.screen.width-10-width)/2;        //获得窗口的水平位置;
-    window.open(url,'video','height='+height+',,innerHeight='+height+',width='+width+',innerWidth='+width+',top='+top+',left='+left+',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
-}
-function popPlay(id){
-    popup('/api/play?id='+id,videoWidth,videoHeight);
-    return false;
-}
